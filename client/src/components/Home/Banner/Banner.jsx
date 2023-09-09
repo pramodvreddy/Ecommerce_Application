d176785ed69aa4f502ba91b4d2b7dabdabc776b8
pramodvreddy <131758537+pramodvreddy@users.noreply.github.com>
2023-09-09 14:18:31 +0530

    Add files via upload

---
+++ client/src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>BASS</h1>
+                    <p>
+                        Experience the sound like never before!!
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={BannerImg} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
